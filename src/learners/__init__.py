53f6b7e4ce6f3a11c476fd2c80f03fcd2ea40474
tdanino12 <36441678+tdanino12@users.noreply.github.com>
2024-08-01 15:41:34 +0300

    Delete src directory

--- src/learners/__init__.py
+++
@@ -1,7 +0,0 @@
-from .q_learner import QLearner
-from .offpg_learner import OffPGLearner
-
-REGISTRY = {}
-
-REGISTRY["q_learner"] = QLearner
-REGISTRY["offpg_learner"] = OffPGLearner
--- src/modules/agents/__init__.py
+++
@@ -1,6 +0,0 @@
-REGISTRY = {}
-
-from .rnn_agent import RNNAgent
-from .grnn_agent import GRNNAgent
-REGISTRY["rnn"] = RNNAgent
-REGISTRY["grnn"] = GRNNAgent
